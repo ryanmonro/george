a63b4fd24f056d955372b4aef193fbdf71e0ab21
ryanmonro <ryan@ryanmonro.info>
2019-10-03 19:26:10 +1000

    Added start button to allow audio on mobile; fixed end of minutes, hours cases

--- app.js
+++ app.js
@@ -1,36 +1,59 @@
 var george = {};
 loadAudio();
-var start = new Date();
-setTimeout(function(){
+
+function start(){
+  document.getElementById('start').style.display = "none";
+  var start = new Date();
+  setTimeout(loop, 1000 - start.getMilliseconds());
+}
+
+function displayTime(now){
+  var timeElement = document.getElementById('time');
+  timeElement.textContent = pad(now.getHours()) + ":" + pad(now.getMinutes()) + ":" + pad(now.getSeconds());
+}
+
+function loop(){
   setInterval(function(){
-    var now = new Date();
-    var timeElement = document.getElementById('time');
+    var now = new Date()
+    displayTime(now);
     var seconds = now.getSeconds()
-    time.textContent = now.toString();
-    if (seconds % 10 > 7 || seconds % 10 == 0) {
+    if (seconds % 10 >= 8 || seconds % 10 == 0) {
       george.stroke.play();
     }
     if (seconds % 10 == 0) {
-      // schedule other audio for the next 10 seconds here
-      // schedule(george.thirdstroke, 5000);
+      now.setSeconds(seconds + 10);
+      seconds = now.getSeconds();
       var start = 8000;
       var minutes = now.getMinutes();
       var hours = now.getHours();
-      if (seconds == 50) {
+      if (seconds == 0) {
         start = scheduleBy(george.precisely, start);
       } else {
         start = scheduleBy(george.seconds, start);
-        start = scheduleBy(george[seconds + 10], start);
+        start = scheduleBy(george[seconds], start);
         start = scheduleBy(george.and, start);
       }
-      start = scheduleBy(george[minutes % 10], start);
-      start = scheduleBy(george[minutes - (minutes % 10)], start);
+      // minutes
+      if (minutes == 0) {
+        start = scheduleBy(george.oclock, start);
+      }
+      if (minutes > 10 && minutes < 20) {
+        start = scheduleBy(george[minutes], start);
+      } else {
+        if (minutes % 10 !== 0) {
+          start = scheduleBy(george[minutes % 10], start);
+        }
+        if (minutes >= 10) {
+          start = scheduleBy(george[minutes - (minutes % 10)], start);
+        }
+      }
+      // hours
       start = scheduleBy(george[hours % 12], start);
       start = scheduleBy(george.thirdstroke, start);
 
     }
   }, 1000)
-}, 1000 - start.getMilliseconds())
+}
 
 function schedule(file, time){
   setTimeout(function(){
@@ -44,10 +67,13 @@ function scheduleBy(file, time){
   return newTime;
 }
 
+function pad(number){
+  return ("0" + number.toString()).slice (-2);
+}
+
 function loadAudio(){
   ['thirdstroke', 'seconds', 'precisely', 'and', 'oclock', 'stroke', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
     20, 30, 40, 50].forEach(function(prefix){
     george[prefix] = new Audio('audio/' + prefix + '.ogg');
   })
-
 }
