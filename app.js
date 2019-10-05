fa39a0dec4acd8a0791e2a2ec30103ff0a2c59cd
ryanmonro <ryan@ryanmonro.info>
2019-10-05 15:51:14 +1000

    Using ToneJS Loop for players

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var george = {};
+var george = new Tone.Players().toMaster();
 
 function start(){
   loadAudio();
@@ -18,7 +18,7 @@ function loop(){
     displayTime(now);
     var seconds = now.getSeconds()
     if (seconds % 10 >= 8 || seconds % 10 == 0) {
-      george.stroke.play();
+      george.get('stroke').start();
     }
     if (seconds % 10 == 0) {
       now.setSeconds(seconds + 10);
@@ -27,24 +27,24 @@ function loop(){
       var minutes = now.getMinutes();
       var hours = now.getHours();
       if (seconds == 0) {
-        start = scheduleBy(george.precisely, start);
+        start = scheduleBy(george.get('precisely'), start);
       } else {
-        start = scheduleBy(george.seconds, start);
-        start = scheduleBy(george[seconds], start);
-        start = scheduleBy(george.and, start);
+        start = scheduleBy(george.get('seconds'), start);
+        start = scheduleBy(george.get(seconds), start);
+        start = scheduleBy(george.get('and'), start);
       }
       // minutes
       if (minutes == 0) {
-        start = scheduleBy(george.oclock, start);
+        start = scheduleBy(george.get('oclock'), start);
       }
       if (minutes > 10 && minutes < 20) {
-        start = scheduleBy(george[minutes], start);
+        start = scheduleBy(george.get(minutes), start);
       } else {
         if (minutes % 10 !== 0) {
-          start = scheduleBy(george[minutes % 10], start);
+          start = scheduleBy(george.get(minutes % 10), start);
         }
         if (minutes >= 10) {
-          start = scheduleBy(george[minutes - (minutes % 10)], start);
+          start = scheduleBy(george.get(minutes - (minutes % 10)), start);
         }
       }
       // hours
@@ -54,8 +54,8 @@ function loop(){
       if (hours > 12) {
         hours = hours % 12;
       }
-      start = scheduleBy(george[hours], start);
-      start = scheduleBy(george.thirdstroke, start);
+      start = scheduleBy(george.get(hours), start);
+      start = scheduleBy(george.get('thirdstroke'), start);
 
     }
   }, 1000)
@@ -63,12 +63,12 @@ function loop(){
 
 function schedule(file, time){
   setTimeout(function(){
-    file.play();
+    file.start();
   }, time);
 }
 
 function scheduleBy(file, time){
-  var newTime = time - (file.duration * 1000);
+  var newTime = time - (file.buffer.duration * 1000);
   schedule(file, newTime);
   return newTime;
 }
@@ -80,6 +80,6 @@ function pad(number){
 function loadAudio(){
   ['thirdstroke', 'seconds', 'precisely', 'and', 'oclock', 'stroke', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
     20, 30, 40, 50].forEach(function(prefix){
-    george[prefix] = new Audio('audio/m4a/' + prefix + '.aif.m4a');
+    george.add(prefix, 'audio/m4a/' + prefix + '.aif.m4a');
   })
 }
